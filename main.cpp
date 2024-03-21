1091c07604b01f358cbfb00470c6ede5c7e502fb
Nguyenybin2015 <nguyenybin2015@gmail.com>
2024-03-21 16:45:00 +0700

    websocket ahihi

--- main.cpp
+++ main.cpp
@@ -1,37 +1,106 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include <curl/curl.h>
 #include <iostream>
-#include <string>
-#include <nlohmann/json.hpp>
-
-static size_t write_callback(void *ptr, size_t size, size_t nmemb, void *userdata) {
-  std::string *data = (std::string*)userdata;
-  data->append((char*)ptr, size * nmemb);
-  return size * nmemb;
+ 
+static int ping(CURL *curl, const char *send_payload)
+{
+  size_t sent;
+  CURLcode result =
+    curl_ws_send(curl, send_payload, strlen(send_payload), &sent, 0,
+                 CURLWS_PING);
+  return (int)result;
 }
-
-int main() {
+ 
+static int recv_pong(CURL *curl, const char *expected_payload)
+{
+  size_t rlen;
+  const struct curl_ws_frame *meta;
+  char buffer[256];
+  CURLcode result = curl_ws_recv(curl, buffer, sizeof(buffer), &rlen, &meta);
+  if(!result) {
+    if(meta->flags & CURLWS_PONG) {
+      int same = 0;
+      fprintf(stderr, "ws: got PONG back\n");
+      if(rlen == strlen(expected_payload)) {
+        if(!memcmp(expected_payload, buffer, rlen)) {
+          fprintf(stderr, "ws: got the same payload back\n");
+          same = 1;
+        }
+      }
+      if(!same)
+        fprintf(stderr, "ws: did NOT get the same payload back\n");
+    }
+    else {
+      fprintf(stderr, "recv_pong: got %u bytes rflags %x\n", (int)rlen,
+              meta->flags);
+    }
+  }
+  fprintf(stderr, "ws: curl_ws_recv returned %u, received %u\n",
+          (unsigned int)result, (unsigned int)rlen);
+  return (int)result;
+}
+ 
+static int recv_any(CURL *curl)
+{
+  size_t rlen;
+  const struct curl_ws_frame *meta;
+  char buffer[256];
+  CURLcode result = curl_ws_recv(curl, buffer, sizeof(buffer), &rlen, &meta);
+  if(result)
+    return result;
+ 
+  return 0;
+}
+ 
+/* close the connection */
+static void websocket_close(CURL *curl)
+{
+  size_t sent;
+  (void)curl_ws_send(curl, "", 0, &sent, 0, CURLWS_CLOSE);
+}
+ 
+static void websocket(CURL *curl)
+{
+  int i = 0;
+  do {
+    recv_any(curl);
+    if(ping(curl, "foobar"))
+      return;
+    if(recv_pong(curl, "foobar")) {
+      return;
+    }
+    sleep(2);
+  } while(i++ < 10);
+  websocket_close(curl);
+}
+ 
+int main(void)
+{
   CURL *curl;
   CURLcode res;
-  std::string url = "ws://localhost:8080"; // ????? URL
-  std::string response;
-
+ 
   curl = curl_easy_init();
-  if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
-
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "ws://localhost:8080");
+ 
+    curl_easy_setopt(curl, CURLOPT_CONNECT_ONLY, 2L); /* websocket style */
+ 
+    /* Perform the request, res gets the return code */
     res = curl_easy_perform(curl);
-    if (res != CURLE_OK) {
-      std::cerr << "Error: " << res << std::endl;
-      std::cerr << "curl_easy_perform failed: " << curl_easy_strerror(res) << std::endl;
-    } else {
-      // ?? JSON
-      std::cout << &response << std::endl;
+    /* Check for errors */
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+    else {
+      /* connected and ready */
+      std::cout << "cho cho bin";
+      websocket(curl);
     }
-
+ 
+    /* always cleanup */
     curl_easy_cleanup(curl);
   }
-
   return 0;
 }
